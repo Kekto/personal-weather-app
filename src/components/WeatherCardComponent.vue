148c98199e7d2568c858754c317cfe6aa3ae3f99
DanPisPol <daniel.piszcz@pollub.edu.pl>
2023-02-27 12:28:29 +0100

    V1.5.2-Fully working animated icons

--- src/components/WeatherCardComponent.vue
+++ src/components/WeatherCardComponent.vue
@@ -11,98 +11,165 @@
     </template>
     <div class="weather-info">
       <div class="weatherCode">
-        <!-- CLEAR SKY DAY/NIGHT-->
-        {{this.getCurrentWeather.current_weather?.weathercode  }}
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 0 &&
-          this.weatherStore.hour >=7 && this.weatherStore.hour <19"
-        >
-          <el-icon size="60px"><Sunny /></el-icon>
+        <!-- ALL DAY WEATHER -->
+        <div>
+          <!-- PARTLY CLOUDY -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 2">
+            <img class="icon" src="@/assets/2.svg"/>
+          </div>
+          <!-- OVERCAST -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 3">
+            <img class="icon" src="@/assets/3.svg"/>
+          </div>
         </div>
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 0 &&
-          this.weatherStore.hour < 7 && this.weatherStore.hour >=19"
-        >
-          <el-icon size="60px"><Moon /></el-icon>
+        <!-- DAY TIME WEATHER -->
+        <div v-if="this.weatherStore.hour >=7 && this.weatherStore.hour <19">
+          <!-- CLEAR -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 0">
+            <img class="icon" src="@/assets/0d.svg"/>
+          </div>
+          <!-- MAINLY CLEAR -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 1">
+            <img class="icon" src="@/assets/1d.svg"/>
+          </div>
+          <!-- FOG -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 45 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 48">
+            <img class="icon" src="@/assets/45d.svg"/>
+          </div>
+          <!-- DRIZZLE -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 51 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 53 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 55">
+            <img class="icon" src="@/assets/51d.svg"/>
+          </div>
+          <!-- FREEZING DRIZZLE -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 56 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 57">
+            <img class="icon" src="@/assets/56d.svg"/>
+          </div>
+          <!-- RAIN -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 61 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 63 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 65">
+            <img class="icon" src="@/assets/61d.svg"/>
+          </div>
+          <!-- FREEZING RAIN -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 66 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 67">
+            <img class="icon" src="@/assets/66d.svg"/>
+          </div>
+          <!-- SNOWFALL & SNOWGRAINS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 71 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 73 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 75 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 77">
+            <img class="icon" src="@/assets/71d.svg"/>
+          </div>
+          <!-- RAIN SHOWERS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 80 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 81 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 82">
+            <img class="icon" src="@/assets/80d.svg"/>
+          </div>
+          <!-- SNOW SHOWERS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 85 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 86">
+            <img class="icon" src="@/assets/85d.svg"/>
+          </div>
+          <!-- THUNDERSTORM -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 95">
+            <img class="icon" src="@/assets/95d.svg"/>
+          </div>
+          <!-- THUNDERSTORM & HAIL -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 96 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 99">
+            <img class="icon" src="@/assets/96d.svg"/>
+          </div>
         </div>
-        <!-- CLOUDY -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 1 "
-        >
-          <el-icon size="60px"><PartlyCloudy /></el-icon>
+        <!-- NIGHT TIME WEATHER -->
+        <div v-if="this.weatherStore.hour < 7 && this.weatherStore.hour >=19">
+          <!-- CLEAR -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 0">
+            <img class="icon" src="@/assets/0n.svg"/>
+          </div>
+          <!-- MAINLY CLEAR -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 1">
+            <img class="icon" src="@/assets/1n.svg"/>
+          </div>
+          <!-- FOG -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 45 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 48">
+            <img class="icon" src="@/assets/45n.svg"/>
+          </div>
+          <!-- DRIZZLE -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 51 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 53 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 55">
+            <img class="icon" src="@/assets/51n.svg"/>
+          </div>
+          <!-- FREEZING DRIZZLE -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 56 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 57">
+            <img class="icon" src="@/assets/56n.svg"/>
+          </div>
+          <!-- RAIN -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 61 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 63 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 65">
+            <img class="icon" src="@/assets/61n.svg"/>
+          </div>
+          <!-- FREEZING RAIN -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 66 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 67">
+            <img class="icon" src="@/assets/66n.svg"/>
+          </div>
+          <!-- SNOWFALL & SNOWGRAINS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 71 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 73 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 75 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 77">
+            <img class="icon" src="@/assets/71n.svg"/>
+          </div>
+          <!-- RAIN SHOWERS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 80 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 81 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 82">
+            <img class="icon" src="@/assets/80n.svg"/>
+          </div>
+          <!-- SNOW SHOWERS -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 85 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 86">
+            <img class="icon" src="@/assets/85n.svg"/>
+          </div>
+          <!-- THUNDERSTORM -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 95">
+            <img class="icon" src="@/assets/95n.svg"/>
+          </div>
+          <!-- THUNDERSTORM & HAIL -->
+          <div v-if="this.getCurrentWeather.current_weather?.weathercode === 96 && 
+                    this.getCurrentWeather.current_weather?.weathercode === 99">
+            <img class="icon" src="@/assets/96n.svg"/>
+          </div>
         </div>
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 2 "
-        >
-          <el-icon size="60px"><MostlyCloudy /></el-icon>
-        </div>
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 3 "
-        >
-          <el-icon size="60px"><Cloudy /></el-icon>
-        </div>
-        <!-- FOG -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 45 ||
-          this.getCurrentWeather.current_weather?.weathercode === 48 "
-        >
-   
-        </div>
-        <!-- DRIZZLING -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 56 ||
-          this.getCurrentWeather.current_weather?.weathercode === 57 "
-        >
-        <el-icon size="60px"><Drizzling /></el-icon>
-        </div>
-        <!-- RAINING -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 61 ||
-          this.getCurrentWeather.current_weather?.weathercode === 62 ||
-          this.getCurrentWeather.current_weather?.weathercode === 63"
-        >
-          <el-icon size="60px"><Pouring /></el-icon>
-        </div>
-        <!-- FREEZING RAIN-->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 66 ||
-          this.getCurrentWeather.current_weather?.weathercode === 67"
-        >
-   
-        </div>
-        <!-- SNOWFALL -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 71 ||
-          this.getCurrentWeather.current_weather?.weathercode === 73 ||
-          this.getCurrentWeather.current_weather?.weathercode === 75 ||
-          this.getCurrentWeather.current_weather?.weathercode === 77"
-        >
-        
-        </div>
-        <!-- RAIN SHOWERS -->
-        <!-- SNOW SHOWERS -->
-        <!-- THUNDERSTORMS -->
-        <div v-if="
-          this.getCurrentWeather.current_weather?.weathercode === 95"
-        >
-          <el-icon size="60px"><Lightning /></el-icon>
-        </div>
-        <!-- HAILS -->
       </div>
       <div class="temperature">
         {{ this.getCurrentWeather.current_weather?.temperature }}°C
       </div>
       <div class="wind-info">
-        <div>
-          Wind Speed: {{ this.getCurrentWeather.current_weather?.windspeed }}
+        <div class="centered">
+          <img style="width: 40px;
+          height: 40px;" src="@/assets/windsock.svg" />
+           {{ this.getCurrentWeather.current_weather?.windspeed }} km/h
         </div>
-        <div>
-          Wind Direction: {{ this.getCurrentWeather.current_weather?.winddirection }}
+        <div class="centered">
+          <img style="width: 40px;
+          height: 40px;" src="@/assets/compass.svg" />
+           {{ this.getCurrentWeather.current_weather?.winddirection }}°
         </div>
       </div>
     </div>
   </el-card>
-
 </template>
   
 <script>
@@ -169,11 +236,21 @@ export default {
 .wind-info{
   display: flex;
   flex-direction: column;
-  gap:20px
+  gap:10px
 }
-
+.centered{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  font-weight: bold;
+}
 .box-card {
   width: 390px;
   border-radius: 15px;
+}
+.icon{
+  width: 100px;
+  height: 100px
 }
 </style>
